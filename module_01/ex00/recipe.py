eb74a5096f52137ad0a2e8cc50c11bc40426121a
sleleu <leleu.sebastien@hotmail.fr>
2023-06-18 22:59:36 +0200

    begin ex00

--- module_01/ex00/recipe.py
+++ module_01/ex00/recipe.py
@@ -5,14 +5,36 @@ class Recipe:
                  name: str,
                  cooking_lvl : int,
                  cooking_time : int,
-                 ingredients : List[str], # List is for python < 3.9
+                 ingredients : List[str],
                  description: str,
                  recipe_type: str):
-        print("yo")
-    
+
+        if cooking_lvl not in range(1, 6):
+            raise ValueError("Cooking level error")
+        if cooking_time < 0:
+            raise ValueError("Bad value for cooking time")
+        if recipe_type not in ["starter","lunch","dessert"]:
+            raise ValueError("Recipe type need to be starter, lunch or dessert")
+
+        self.name = name
+        self.cooking_lvl = cooking_lvl
+        self.cooking_time = cooking_time
+        self.ingredients = ingredients
+        self.description = description
+        self.recipe_type = recipe_type
+        
+
     def __str__(self):
-        str = "teeest"
+        str = f"Recipe {self.name}:\n\
+    - cooking_lvl: {self.cooking_lvl}\n\
+    - cooking time: {self.cooking_time} minutes\n\
+    - ingredients: {self.ingredients}\n\
+    - description: {self.description}\n\
+    - recipe_type: {self.recipe_type}\n"
         return str
 
-tourte = Recipe("sd", 1, 10, ["tomatoes", "chips"], "description here", "lunch")
-print(tourte)
+try:
+    tourte = Recipe("tourte", 5, 10, ["tomatoes", "chips"], "description here", "lunch")
+    print(tourte)
+except ValueError as Error:
+    print(Error)
